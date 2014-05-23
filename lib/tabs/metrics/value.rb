4720b77a130e666aff6ca09a836aa85b759537f8
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-05-23 16:53:03 +0200

    force another int conversion

--- lib/tabs/metrics/value.rb
+++ lib/tabs/metrics/value.rb
@@ -84,7 +84,7 @@ module Tabs
       end
 
       def decrement(hash, value)
-        if  (hash["sum"] - value) >= 0 || Tabs::Config.negative_metric
+        if  (hash["sum"] - value.to_i) >= 0 || Tabs::Config.negative_metric
          hash["count"] -= 1
          hash["sum"] -= value.to_i
       end
